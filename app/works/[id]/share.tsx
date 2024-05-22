937d5880f1361170e5f9dbcb83244de658c6783d
Abner Rios <46605063+abn3rr@users.noreply.github.com>
2024-05-22 11:32:34 +0200

    feat: add watermark

--- app/works/[id]/share.tsx
+++ app/works/[id]/share.tsx
@@ -6,6 +6,11 @@ import { useWorkByIdQuery } from "@/data/hooks/useWorkByIdQuery";
 import { LoadingShade } from "@/components/LoadingShade";
 import * as Sharing from "expo-sharing";
 import ImagePicker from "react-native-image-crop-picker";
+import Marker, {
+  Position,
+  TextBackgroundType,
+  ImageFormat,
+} from "react-native-image-marker";
 
 
 function normalizeFilePath(path: string) {
@@ -35,8 +40,38 @@ export default function ShareWork() {
       height: 300,
       mediaType: "photo",
     });
-    setEditedImagePath(normalizeFilePath(image.path));
+    
+    const markedImagePath = await Marker.markText({
+      backgroundImage: {
+        src: image.path,
+        scale: 1,
+      },
+      watermarkTexts: [
+        {
+          text: "#cma",
+          position: {
+            position: Position.bottomRight,
+          },
+          style: {
+            color: "#fff",
+            fontSize: 20,
+            textBackgroundStyle: {
+              type: TextBackgroundType.none,
+              color: "#000",
+              paddingX: 16,
+              paddingY: 6,
+            },
+          },
+        },
+      ],
+      quality: 100,
+      filename: image.filename,
+      saveFormat: ImageFormat.jpg,
+    });
+    
+    setEditedImagePath(normalizeFilePath(markedImagePath));
   }
+
 
   return (
     <View className="flex-1 bg-shade-1">
